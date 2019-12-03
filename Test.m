9a95b35a78d1b6ccc72cfecd9ebf75213e8f5215
Ryan <davry101@student.otago.ac.nz>
2019-12-04 10:20:28 +1300

    Test file works, Poisson.m seems to be giving correct output

--- Test.m
+++ Test.m
@@ -1,7 +1,7 @@
-LX = 1;
-LY = 1;
-NX = 1024;
-NY = 1024;
+LX = 2*pi;
+LY = 2*pi;
+NX = 128;
+NY = 128;
 I = sqrt(-1);
 
 a = linspace(0,LX,NX);
@@ -14,14 +14,21 @@ dealias = abs(KX) < (1/3)*NX & abs(KY) < (1/3)*NY;
 
 [A,B] = ndgrid(a,b);
 
-Aexp = exp(A.^2 + B.^2);
-Bsin = sin(A + B);
+Asin = sin(A + B);
+Bsin = sin(A - B);
 
-Ahat = fft2(Aexp);
+Ahat = fft2(Asin);
 Bhat = fft2(Bsin);
+
 PBhat = Poisson(Ahat, Bhat, KX, KY);
 
-%analytic = 2*
+analytic = -cos(2*B)-cos(2*A);
 
-PB = real(ifft2(PBhat).*dealias);
-plot3(a,b,Aexp)
+PB = real(ifft2(PBhat.*dealias));
+
+subplot(1,2,1)
+surf(PB)
+pbaspect([1 1 1])
+subplot(1,2,2)
+surf(analytic-PB)
+pbaspect([1 1 1])
