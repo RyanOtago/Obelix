3ab083478da2257b7c35062025ff90053e0ace61
Ryan <davry101@student.otago.ac.nz>
2020-01-20 16:25:28 +1300

    'updated' dispersion checkers. Not sure of this is the right track at all

--- RMHD_3D_Dispersion.m
+++ RMHD_3D_Dispersion.m
@@ -1,4 +1,4 @@
-function [omegaout] = RMHD_3D_Dispersion(kn, LX, NX, bpar, va, nu, TF, no_perp, no_alfven, strengthpx, strengthmx, strengthpy, strengthmy)
+function [omegaout] = RMHD_3D_Dispersion(kn, LX, NX, bpar, va, nu, TFrun, no_perp, no_alfven, strengthpx, strengthmx, strengthpy, strengthmy)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%                               %%%
@@ -11,27 +11,36 @@ function [omegaout] = RMHD_3D_Dispersion(kn, LX, NX, bpar, va, nu, TF, no_perp, 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Paramaters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Cutoff = 1000000;
+CFL    = 0.13;      % Courant Number
+
 LY = LX;     % Box-size (y-direction)
 LZ = LX;     % Box-size (z-direction)     %%% !!! Should this scale differently to LX and LY? !!! %%%
 
 NY = NX./2;      % Resolution in y
 NZ = NX./2;      % Resolution in z
 
-for strengthnn = 1:length(strengthpx)
+for stnn = 1:length(strengthpx)
     for bparnn = 1:length(bpar)
         for meshnn = 1:length(NX)
             for knnn = 1:length(kn)
                 
                 N = NX(meshnn)*NY(meshnn)*NZ(meshnn);
-                dt  = TF(bparnn,knnn)/(1e4);      % Time Step
-                time = dt:dt:TF(bparnn,knnn);
-                E_zeta_plus  = zeros(1,length(time));
-                E_zeta_minus = zeros(1,length(time));
-                E_s_plus     = zeros(1,length(time));
-                E_s_minus    = zeros(1,length(time));
-                u_par_save   = zeros(1,length(time));
-                u_perx_save  = zeros(1,length(time));
-                u_pery_save  = zeros(1,length(time));
+                TF = TFrun(bparnn, knnn, stnn);
+                TScreen = 100;
+                SlowModes = 1;
+                Fullscreen = 1;
+                
+                time         = zeros(1, Cutoff+1);    % +1 accounts for t=0 (incase we reach the cutoff)
+                dt_save      = zeros(1, Cutoff);
+                E_z_plus     = zeros(1, Cutoff);
+                E_z_minus    = zeros(1, Cutoff);
+                E_s_plus     = zeros(1, Cutoff);
+                E_s_minus    = zeros(1, Cutoff);
+                u_par_save   = zeros(1, Cutoff);
+                u_perx_save  = zeros(1, Cutoff);
+                u_pery_save  = zeros(1, Cutoff);
                 
                 dx = LX(knnn)/NX(meshnn);
                 dy = LY(knnn)/NY(meshnn);
@@ -48,25 +57,42 @@ for strengthnn = 1:length(strengthpx)
                 ky = (2*I*pi/LY(knnn))*[0:((NY(meshnn)/2)-1)  -(NY(meshnn)/2):-1];
                 kz = (2*I*pi/LZ(knnn))*[0:((NZ(meshnn)/2)-1)  -(NZ(meshnn)/2):-1];
                 [KX, KY, KZ] = ndgrid(kx, ky, kz);
-                
+                idmat = zeros(size(KX));
+                idmat(1,1,1) = 1;
                 dealias = abs(KX)<(1/3)*NX(meshnn) & abs(KY)<(1/3)*NY(meshnn) & abs(KZ)<(1/3)*NZ(meshnn);    % Cutting of frequencies for dealiasing using the 2/3 rule   (2/3)*(N/2)
                 
                 k2_perp = KX.^2 + KY.^2;      % (Perpendicular) Laplacian in Fourier space
                 k2_poisson = k2_perp;
                 k2_poisson(1,1,:) = 1;        % Fixed Laplacian in F.S. for Poisson's equation (Because first entry was 0)
                 k2 = k2_perp + KZ.^2;         % Laplacian in F.S.
+                kperpmax = max([abs(kx) abs(ky)]);
+                kzmax    = max(abs(kz));
+                dt = min(CFL/(kzmax*va), TF/1e4);
+%                 disp(TF/dt)
+%                 dt = TF/1e4
+                exp_correct = exp(dt*nu*k2);  % Romain thinks k2 %%%                        !!! Can include linear term !!!
                 
                 %% Initial Condition %%%%%%%%%%%%%%%%%%%%%%%%%%%
                 
                 [i,j,k] = ndgrid((1:NX(meshnn))*dx,(1:NY(meshnn))*dy,(1:NZ(meshnn))*dz);
+                XG = permute(i, [2 1 3]);
+                YG = permute(j, [2 1 3]);
+                ZG = permute(k, [2 1 3]);
                 
-                if no_alfven == 0
-                    Lap_zeta_plus  = k2_perp.*fftn(sin(kn(knnn)*i));
-                    Lap_zeta_minus = k2_perp.*fftn(cos(kn(knnn)*(i+j)));
-                else
-                    Lap_zeta_plus  = k2_perp.*fftn(0);
-                    Lap_zeta_minus = k2_perp.*fftn(0);
-                end
+                stpx = (idmat*strengthpx(stnn));
+                stpy = (idmat*strengthpy(stnn));
+                stmx = (idmat*strengthmx(stnn));
+                stmy = (idmat*strengthmy(stnn));
+                
+%                 if no_alfven == 0
+%                     Lap_z_plus  = k2_perp.*fftn(sin(kn(knnn)*i));
+%                     Lap_z_minus = k2_perp.*fftn(cos(kn(knnn)*(i+j)));
+%                 else
+%                     Lap_z_plus  = k2_perp.*fftn(0);
+%                     Lap_z_minus = k2_perp.*fftn(0);
+%                 end
+                
+                
                 
                 if no_perp == 1
                     s_plus  = fftn(0.01*sin(kn(knnn)*k));
@@ -75,38 +101,59 @@ for strengthnn = 1:length(strengthpx)
                     s_plus  = fftn(0.01*sin((kn(knnn))*(i + j + k)));
                     s_minus = fftn(0);
                 end
-                exp_correct = exp(dt*nu*k2);
+                
                 
                 
                 %% Solver %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                 
-                k=0;
+                k=1;
                 n=1;
-                for i = (1:length(time))
-                    k=k+1;
-                    
-                    
+                m=0;
+                
+                
+%                 zeta_plus = Lap_z_plus./k2_poisson;
+%                 zeta_minus = Lap_z_minus./k2_poisson;
+                
+                %%% Compute Poisson Brackets
+                
+                % Calculates derivatives for PB
+                if no_perp == 0 && no_alfven == 0
+                    zp_x  = real(ifftn(stpx));
+                    zm_x  = real(ifftn(stmx));
+                    zp_y  = real(ifftn(stpy));
+                    zm_y  = real(ifftn(stmy));
+                else
+                    zp_x  = real(ifftn(KX.*zeta_plus));
+                    zm_x  = real(ifftn(KX.*zeta_minus));
+                    zp_y  = real(ifftn(KY.*zeta_plus));
+                    zm_y  = real(ifftn(KY.*zeta_minus));
+                end
+                
+%                 Lzp_x = real(ifftn(KX.*Lap_z_plus));
+%                 Lzm_x = real(ifftn(KX.*Lap_z_minus));
+%                 Lzp_y = real(ifftn(KY.*Lap_z_plus));
+%                 Lzm_y = real(ifftn(KY.*Lap_z_minus));
+                Lap_z_plus  = KX.*stpx + KY.*stpy;
+                Lap_z_minus = KX.*stmx + KY.*stmy;
+
+
+                Lzp_x = real(ifftn(KX.*Lap_z_plus));
+                Lzm_x = real(ifftn(KX.*Lap_z_minus));
+                Lzp_y = real(ifftn(KY.*Lap_z_plus));
+                Lzm_y = real(ifftn(KY.*Lap_z_minus));
+                
+                
+                while t<TF && n<Cutoff
+                    %                     tic
                     %%% Update zeta p/m for new time-step
-                    %                 zeta_plus = Lap_zeta_plus./k2_poisson;
-                    %                 zeta_minus = Lap_zeta_minus./k2_poisson;
                     
-                    %%% Compute Poisson Brackets
-                    
-                    % Calculates derivatives for PB
-                    zp_x  = strengthpx(strengthnn);%real(ifftn(KX.*zeta_plus));
-                    zm_x  = strengthmx(strengthnn);%real(ifftn(KX.*zeta_minus));
-                    zp_y  = strengthpy(strengthnn);%real(ifftn(KY.*zeta_plus));
-                    zm_y  = strengthmy(strengthnn);%real(ifftn(KY.*zeta_minus));
                     
                     sp_x  = real(ifftn(KX.*s_plus));
                     sm_x  = real(ifftn(KX.*s_minus));
                     sp_y  = real(ifftn(KY.*s_plus));
                     sm_y  = real(ifftn(KY.*s_minus));
                     
-                    Lzp_x = real(ifftn(KX.*Lap_zeta_plus));
-                    Lzm_x = real(ifftn(KX.*Lap_zeta_minus));
-                    Lzp_y = real(ifftn(KY.*Lap_zeta_plus));
-                    Lzm_y = real(ifftn(KY.*Lap_zeta_minus));
+
                     
                     % Calculates PB
                     % Alfven
@@ -129,20 +176,30 @@ for strengthnn = 1:length(strengthpx)
                     
                     %%% Compute Linear terms
                     
-                    Lin_zeta_plus  =  va.*KZ.*Lap_zeta_plus;
-                    Lin_zeta_minus = -va.*KZ.*Lap_zeta_minus;
+                    Lin_zeta_plus  =  va.*KZ.*Lap_z_plus;
+                    Lin_zeta_minus = -va.*KZ.*Lap_z_minus;
                     Lin_s_plus     = (va*bpar(bparnn)).*KZ.*s_plus;
                     Lin_s_minus    = -(va*bpar(bparnn)).*KZ.*s_minus;
                     
                     %%% Compute Solution at the next step %%%
-                    
-                    Lap_zeta_plus_new  = 0*dt*(Lin_zeta_plus + NL_zeta_plus) + Lap_zeta_plus;
-                    Lap_zeta_minus_new = 0*dt*(Lin_zeta_minus + NL_zeta_minus) + Lap_zeta_minus;
+                    if t == dt
+                        Lap_zeta_plus_new  = dt*(Lin_zeta_plus + NL_zeta_plus);% + Lap_z_plus;
+                        Lap_zeta_minus_new = dt*(Lin_zeta_minus + NL_zeta_minus);% + Lap_z_minus;
+                    else
+                        Lap_zeta_plus_new  = 0*dt*(Lin_zeta_plus + NL_zeta_plus) + Lap_z_plus;
+                        Lap_zeta_minus_new = 0*dt*(Lin_zeta_minus + NL_zeta_minus) + Lap_z_minus;
+                        
+                    end
                     s_plus_new         = dt*(Lin_s_plus + NL_s_plus) + s_plus;
                     s_minus_new        = dt*(Lin_s_minus + NL_s_minus) + s_minus;
                     
-                    Lap_zeta_plus_new  = Lap_zeta_plus_new.*exp_correct;
-                    Lap_zeta_minus_new = Lap_zeta_minus_new.*exp_correct;
+                    %                     Lap_zeta_plus_new  = Lap_zeta_plus_new.*exp_correct;
+                    %
+                    %                     %%% Forgot to disable this, so field was changing
+                    %                     over time. Could have (probably) screwed with smallscale
+                    %                     wavenumbers
+                    %
+                    %                     Lap_zeta_minus_new = Lap_zeta_minus_new.*exp_correct;
                     s_plus_new  = s_plus_new.*exp_correct;
                     s_minus_new = s_minus_new.*exp_correct;
                     
@@ -160,6 +217,132 @@ for strengthnn = 1:length(strengthpx)
                     
                     t=t+dt;
                     
+                    %% Plotting %%%
+                    if (k == TScreen) && m == 0
+                        
+                        %Go back to real space for plotting
+                        zetap = Lap_zeta_plus_new./k2_poisson;
+                        zetam = Lap_zeta_minus_new./k2_poisson;
+                        zpx = KX.*zetap;
+                        zpy = KY.*zetap;
+                        zmx = KX.*zetam;
+                        zmy = KY.*zetam;
+                        zp  = double(permute(real(ifftn(0.5*(- zpy - zmy))),[2,1,3]));
+                        zm  = double(permute(real(ifftn(0.5*(  zpx + zmx))),[2,1,3]));
+                        disp([mean(zp(:)) mean(zm(:))])
+                        disp([(strengthpy(stnn)-strengthmy(stnn)) strengthpx(stnn)+strengthmx(stnn)])
+%                         disp(zpx)
+% %                                                 zp  = double(permute(real(ifftn(Lap_zeta_plus_new./k2_poisson)),[2,1,3]));
+% %                                                 zm  = double(permute(real(ifftn(Lap_zeta_minus_new./k2_poisson)),[2,1,3]));
+%                         if SlowModes == 1
+%                             sp     = double(permute(real(ifftn(s_plus_new)),[2,1,3]));
+%                             sm     = double(permute(real(ifftn(s_minus_new)),[2,1,3]));
+%                         end
+%                         figure(1)
+%                         if Fullscreen == 1
+%                             set(gcf, 'Units', 'Normalized', 'OuterPosition', [0, 0.04, 1, 0.96])        % Makes figure fullscreen
+%                         end
+%                         if SlowModes == 1
+%                             subplot(2,2,1)
+%                         else
+%                             subplot(1,2,1)
+%                         end
+%                         hold on
+%                         hx = slice(XG, YG, ZG, zp, LX, [], []);
+%                         set(hx,'FaceColor','interp','EdgeColor','none')
+%                         hy = slice(XG, YG, ZG, zp, [], dy, []);
+%                         set(hy,'FaceColor','interp','EdgeColor','none')
+%                         hz = slice(XG, YG, ZG, zp, [], [], LZ);
+%                         set(hz,'FaceColor','interp','EdgeColor','none')
+%                         hold off
+%                         daspect([1,1,1])
+%                         axis tight
+%                         box on
+%                         view(42,16)
+%                         camproj perspective
+%                         set(gcf,'Renderer','zbuffer')
+%                         title([num2str(t,'%0.3f') '  \zeta^+'])
+%                         xlabel('x')
+%                         ylabel('y')
+%                         zlabel('z')
+%                         colorbar
+%                         
+%                         if SlowModes == 1
+%                             subplot(2,2,2)
+%                         else
+%                             subplot(1,2,2)
+%                         end
+%                         hold on
+%                         hx = slice(XG, YG, ZG, zm, LX, [], []);
+%                         set(hx,'FaceColor','interp','EdgeColor','none')
+%                         hy = slice(XG, YG, ZG, zm, [], dy, []);
+%                         set(hy,'FaceColor','interp','EdgeColor','none')
+%                         hz = slice(XG, YG, ZG, zm, [], [], LZ);
+%                         set(hz,'FaceColor','interp','EdgeColor','none')
+%                         hold off
+%                         daspect([1,1,1])
+%                         axis tight
+%                         box on
+%                         view(42,16)
+%                         camproj perspective
+%                         set(gcf,'Renderer','zbuffer')
+%                         title('\zeta^-')
+%                         xlabel('x')
+%                         ylabel('y')
+%                         zlabel('z')
+%                         colorbar
+%                         
+%                         if SlowModes == 1
+%                             subplot(2,2,3)
+%                             hold on
+%                             hx = slice(XG, YG, ZG, sp, LX, [], []);
+%                             set(hx,'FaceColor','interp','EdgeColor','none')
+%                             hy = slice(XG, YG, ZG, sp, [], dy, []);
+%                             set(hy,'FaceColor','interp','EdgeColor','none')
+%                             hz = slice(XG, YG, ZG, sp, [], [], LZ);
+%                             set(hz,'FaceColor','interp','EdgeColor','none')
+%                             hold off
+%                             
+%                             daspect([1,1,1])
+%                             axis tight
+%                             box on
+%                             view(42,16)
+%                             camproj perspective
+%                             set(gcf,'Renderer','zbuffer')
+%                             title('z^+')
+%                             xlabel('x')
+%                             ylabel('y')
+%                             zlabel('z')
+%                             colorbar
+%                             
+%                             subplot(2,2,4)
+%                             hold on
+%                             hx = slice(XG, YG, ZG, sm, LX, [], []);
+%                             set(hx,'FaceColor','interp','EdgeColor','none')
+%                             hy = slice(XG, YG, ZG, sm, [], dy, []);
+%                             set(hy,'FaceColor','interp','EdgeColor','none')
+%                             hz = slice(XG, YG, ZG, sm, [], [], LZ);
+%                             set(hz,'FaceColor','interp','EdgeColor','none')
+%                             hold off
+%                             
+%                             daspect([1,1,1])
+%                             axis tight
+%                             box on
+%                             view(42,16)
+%                             camproj perspective
+%                             set(gcf,'Renderer','zbuffer')
+%                             title('z^-')
+%                             xlabel('x')
+%                             ylabel('y')
+%                             zlabel('z')
+%                             colorbar
+%                         end
+%                         saveas(gcf, ['./gif/' num2str(t) '.jpg'])
+                        drawnow
+                        k=0;
+                        m=1;
+                    end
+                    
                     u_par = real(ifftn((0.5).*(s_plus_new + s_minus_new)));
                     u_par_save(n)  =  u_par(NX(meshnn)/2, NY(meshnn)/2, NZ(meshnn)/2);
                     
@@ -168,15 +351,16 @@ for strengthnn = 1:length(strengthpx)
                     phi_y = real(ifftn(KY.*phi)).*dealias;    %-x component of u_perp
                     u_perx_save(n) = -phi_y(NX(meshnn)/2, NY(meshnn)/2, NZ(meshnn)/2);
                     u_pery_save(n) =  phi_x(NX(meshnn)/2, NY(meshnn)/2, NZ(meshnn)/2);
-                    
+%                     disp(toc)
+                    k=k+1;
                     n=n+1;
-                    Lap_zeta_plus  = Lap_zeta_plus_new;
-                    Lap_zeta_minus = Lap_zeta_minus_new;
+                    Lap_z_plus  = Lap_zeta_plus_new;
+                    Lap_z_minus = Lap_zeta_minus_new;
                     s_plus         = s_plus_new;
                     s_minus        = s_minus_new;
                 end
                 %% Calculate omega %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                
+%                 disp(n)
                 [velxmax, tvx_max] = findpeaks(abs(u_perx_save),'MinPeakProminence',0.2e-12);
                 
                 [velymax, tvy_max] = findpeaks(abs(u_pery_save),'MinPeakProminence', 0.2e-12);
@@ -210,18 +394,18 @@ for strengthnn = 1:length(strengthpx)
                 end
                 
                 if isnan(omega) == 0
-                    w(bparnn,knnn,meshnn) = omega;
+                    w(bparnn,knnn,stnn) = omega;
                 else
-                    w(bparnn,knnn,meshnn) = 0;
+                    w(bparnn,knnn,stnn) = 0;
                 end
+                %                 disp(TF - t)
                 disp([' - Just Finished Wavenumber: ' num2str(kn(knnn))])
                 
             end
         end
         disp(['Just Finished bpar = ' num2str(bpar(bparnn))])
     end
-    disp(['Just finished strength = ' num2str(strength(strengthnn))])
+    disp(['Just finished strength #' num2str(stnn)])
 end
 omegaout = w;
-
 end
--- Run_RMHD.m
+++ Run_RMHD.m
@@ -1,4 +1,5 @@
 function Run_RMHD
+clear all
 
 % Automatically cycles through wavenumbers to test the dispersion
 % relationship of linear waves propagating through a 3D torus
@@ -7,16 +8,24 @@ no_alfven = 0;      % Only test w = v_a*bpar*k_parallel (Makes u_perp and B_perp
 no_perp   = 0;      % Only test z-axis propagation
 va   = 1;
 nu = 1e-3;
-kn = (2*pi)*[0.1, 1, 10, 20, 50, 100];
+kn = (2*pi)*[1, 10];%, 50, 100];%, 500, 1000];
 NX = 32;        % Mesh resolution
-beta = [1];
-stpx = [0.1, 0.1, 1, 0,  10];
-stmx = [0.1, 0.1, 3, 0, -10];
-stpy = [0.1, 0  , 0, 10, 10];   
-stmy = [0.1, 0  , 0, 10, 0];
+beta = [1];%, 5, 10];%, 10];%, 10];%[0.01, 0.1, 1, 10, 100];
+
+if no_alfven == 0 && no_perp == 0
+    stpx = [10];%, 0];%, 10];%,  10];          % Note max of 7 (unless add more colours to 'colour' vector)
+    stmx = [0];%, 0];%, 10];%,  10];
+    stpy = [0];%, -2];%,  0];%,  10];
+    stmy = [0];%, 2];%,  0];%,   0];
+else
+    stpx = 1;
+    stmx = 1;
+    stpy = 1;
+    stmy = 1;
+end
 
 bpar = 1./sqrt(1+(1./beta).^2);
-        
+
 
 if no_alfven == 1
     disp('RMHD Dispersion Test !! No Alfven !!')
@@ -25,11 +34,11 @@ if no_alfven == 1
     if no_perp == 1
         disp('!! z-axis propagation only !!')
         LX = (2*pi)./(kn);
-        [omega] = RMHD_3D_Dispersion(kn, LX, NX, bpar, va, nu, TF, no_perp, no_alfven);
+        [omega] = RMHD_3D_Dispersion(kn, LX, NX, bpar, va, nu, TF, no_perp, no_alfven, stpx, stmx, stpy, stmy);
     else
         disp('Diagonal Propagation')
         LX = (2*pi)./kn;
-        [omega] = RMHD_3D_Dispersion(kn, LX, NX, bpar, va, nu, TF, no_perp, no_alfven);
+        [omega] = RMHD_3D_Dispersion(kn, LX, NX, bpar, va, nu, TF, no_perp, no_alfven, stpx, stmx, stpy, stmy);
     end
 else
     disp('RMHD Dispersion Test with Alfven Waves')
@@ -38,36 +47,56 @@ else
         disp('!! z-axis propagation only !!')
         TF = 6*pi./(va.*(bpar')*kn);
         LX = (2*pi)./(kn);
-        [omega] = RMHD_3D_Dispersion(kn, LX, NX, bpar, va, nu, TF, no_perp, no_alfven);
+        [omega] = RMHD_3D_Dispersion(kn, LX, NX, bpar, va, nu, TF, no_perp, no_alfven, stpx, stmx, stpy, stmy);
     else
         disp('Diagonal Propagation')
-        TF = 6*pi./((kn/(2*sqrt(3))) + va.*(bpar')*kn);
+        for ii = 1:length(stpx)
+            for jj = 1:length(kn)
+                for kk = 1:length(bpar)
+                    TF(kk, jj, ii) = 6*pi./((kn(jj)/(2*sqrt(3)))*(stpx(ii) + stmx(ii) - stpy(ii) - stmy(ii)) + va*bpar(kk)*kn(jj) + (bpar(kk)*kn(jj))/(2*sqrt(3))*(stpx(ii) + stmy(ii) - stmx(ii) - stpy(ii)));
+                end
+            end
+        end
         LX = (2*pi)./kn;
         [omega] = RMHD_3D_Dispersion(kn, LX, NX, bpar, va, nu, TF, no_perp, no_alfven, stpx, stmx, stpy, stmy);
     end
 end
 
-
 %% Analytic Dispersion
 
-for meshnn = 1:length(NX)
-    if no_alfven == 1
-        kAn = linspace(kn(1),kn(end),100);
+if no_alfven == 1
+    kAn = linspace(kn(1),kn(end));
+    omegaAn = va.*(bpar')*kAn;
+    plot(kAn, omegaAn, kn, squeeze(omega), 'o')
+    legend(num2str(beta'))
+else
+    if no_perp == 1
+        kAn = linspace(kn(1),kn(end));
         omegaAn = va.*(bpar')*kAn;
         plot(kAn, omegaAn, kn, squeeze(omega), 'o')
         legend(num2str(beta'))
     else
-        if no_perp == 1
-            kAn = linspace(kn(1),kn(end),100);
-            omegaAn = va.*(bpar')*kAn;
-            plot(kAn, omegaAn, kn, squeeze(omega), 'o')
-            legend(num2str(beta'))
-        else
-            kAn = linspace(kn(1),kn(end),100);
-            omegaAn = va.*(bpar')*kAn;
-            plot(kAn, omegaAn, kn, squeeze(omega), 'o')
-            legend(num2str(beta'))
-            return
+        kAn = linspace(kn(1),kn(end));
+        for ii = 1:length(stpx)
+            for jj = 1:length(kAn)
+                for kk = 1:length(bpar)
+                    omegaAn(kk, jj, ii) = ((kAn(jj)/(2*sqrt(3)))*(stpx(ii) + stmx(ii) - stpy(ii) - stmy(ii)) + va*bpar(kk)*kAn(jj) + (bpar(kk)*kAn(jj))/(2*sqrt(3))*(stpx(ii) + stmy(ii) - stmx(ii) - stpy(ii)));
+                end
+            end
         end
+    end
+end
+
+if no_alfven == 0 && no_perp == 0
+    colourAn = {'b-', 'r-', 'm-', 'y-', 'c-', 'g-', 'k-'};
+    colour   = {'bo', 'ro', 'mo', 'yo', 'co', 'go', 'ko'};
+    close all
+    figure(1)
+    hold on
+    for kk = 1:length(bpar)
+        subplot(1, length(bpar), kk)
+        plot(kAn, squeeze(omegaAn(kk, :, :)), kn, squeeze(omega(kk, :, :)),'o')
+        legend(num2str([1:length(stpx)]'), 'Location', 'Best')
+        title(num2str(bpar(kk)))
     end
 end
