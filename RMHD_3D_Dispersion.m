18ef40c4477b7383f7a97b30a587e7f2c9408fd7
Ryan <davry101@student.otago.ac.nz>
2020-01-21 12:01:02 +1300

    Removed Alfven evolution terms from dispersion test branch

--- RMHD_3D_Dispersion.m
+++ RMHD_3D_Dispersion.m
@@ -28,7 +28,7 @@ for stnn = 1:length(strengthpx)
                 
                 N = NX(meshnn)*NY(meshnn)*NZ(meshnn);
                 TF = TFrun(bparnn, knnn, stnn);
-                TScreen = 100;
+                TScreen = 0;
                 SlowModes = 1;
                 Fullscreen = 1;
                 
@@ -57,8 +57,6 @@ for stnn = 1:length(strengthpx)
                 ky = (2*I*pi/LY(knnn))*[0:((NY(meshnn)/2)-1)  -(NY(meshnn)/2):-1];
                 kz = (2*I*pi/LZ(knnn))*[0:((NZ(meshnn)/2)-1)  -(NZ(meshnn)/2):-1];
                 [KX, KY, KZ] = ndgrid(kx, ky, kz);
-                idmat = zeros(size(KX));
-                idmat(1,1,1) = 1;
                 dealias = abs(KX)<(1/3)*NX(meshnn) & abs(KY)<(1/3)*NY(meshnn) & abs(KZ)<(1/3)*NZ(meshnn);    % Cutting of frequencies for dealiasing using the 2/3 rule   (2/3)*(N/2)
                 
                 k2_perp = KX.^2 + KY.^2;      % (Perpendicular) Laplacian in Fourier space
@@ -78,22 +76,7 @@ for stnn = 1:length(strengthpx)
                 XG = permute(i, [2 1 3]);
                 YG = permute(j, [2 1 3]);
                 ZG = permute(k, [2 1 3]);
-                
-                stpx = (idmat*strengthpx(stnn));
-                stpy = (idmat*strengthpy(stnn));
-                stmx = (idmat*strengthmx(stnn));
-                stmy = (idmat*strengthmy(stnn));
-                
-%                 if no_alfven == 0
-%                     Lap_z_plus  = k2_perp.*fftn(sin(kn(knnn)*i));
-%                     Lap_z_minus = k2_perp.*fftn(cos(kn(knnn)*(i+j)));
-%                 else
-%                     Lap_z_plus  = k2_perp.*fftn(0);
-%                     Lap_z_minus = k2_perp.*fftn(0);
-%                 end
-                
-                
-                
+        
                 if no_perp == 1
                     s_plus  = fftn(0.01*sin(kn(knnn)*k));
                     s_minus = fftn(0);
@@ -102,116 +85,48 @@ for stnn = 1:length(strengthpx)
                     s_minus = fftn(0);
                 end
                 
-                
-                
                 %% Solver %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                 
                 k=1;
                 n=1;
                 m=0;
-                
-                
-%                 zeta_plus = Lap_z_plus./k2_poisson;
-%                 zeta_minus = Lap_z_minus./k2_poisson;
-                
+                               
                 %%% Compute Poisson Brackets
                 
-                % Calculates derivatives for PB
-                if no_perp == 0 && no_alfven == 0
-                    zp_x  = real(ifftn(stpx));
-                    zm_x  = real(ifftn(stmx));
-                    zp_y  = real(ifftn(stpy));
-                    zm_y  = real(ifftn(stmy));
-                else
-                    zp_x  = real(ifftn(KX.*zeta_plus));
-                    zm_x  = real(ifftn(KX.*zeta_minus));
-                    zp_y  = real(ifftn(KY.*zeta_plus));
-                    zm_y  = real(ifftn(KY.*zeta_minus));
-                end
-                
-%                 Lzp_x = real(ifftn(KX.*Lap_z_plus));
-%                 Lzm_x = real(ifftn(KX.*Lap_z_minus));
-%                 Lzp_y = real(ifftn(KY.*Lap_z_plus));
-%                 Lzm_y = real(ifftn(KY.*Lap_z_minus));
-                Lap_z_plus  = KX.*stpx + KY.*stpy;
-                Lap_z_minus = KX.*stmx + KY.*stmy;
-
-
-                Lzp_x = real(ifftn(KX.*Lap_z_plus));
-                Lzm_x = real(ifftn(KX.*Lap_z_minus));
-                Lzp_y = real(ifftn(KY.*Lap_z_plus));
-                Lzm_y = real(ifftn(KY.*Lap_z_minus));
-                
-                
                 while t<TF && n<Cutoff
-                    %                     tic
-                    %%% Update zeta p/m for new time-step
-                    
-                    
+      
                     sp_x  = real(ifftn(KX.*s_plus));
                     sm_x  = real(ifftn(KX.*s_minus));
                     sp_y  = real(ifftn(KY.*s_plus));
                     sm_y  = real(ifftn(KY.*s_minus));
                     
-
-                    
                     % Calculates PB
-                    % Alfven
-                    PB_zp_Lzm = fftn((zp_x.*Lzm_y) - (zp_y.*Lzm_x));
-                    PB_zm_Lzp = fftn((zm_x.*Lzp_y) - (zm_y.*Lzp_x));
-                    PB_zp_zm  = fftn((zp_x.*zm_y)  - (zp_y.*zm_x));
                     % Compressive
-                    PB_zp_sp  = fftn((zp_x.*sp_y)  - (zp_y.*sp_x));
-                    PB_zp_sm  = fftn((zp_x.*sm_y)  - (zp_y.*sm_x));
-                    PB_zm_sp  = fftn((zm_x.*sp_y)  - (zm_y.*sp_x));
-                    PB_zm_sm  = fftn((zm_x.*sm_y)  - (zm_y.*sm_x));
-                    
-                    NL_zeta_Sup = -(0.5).*(PB_zp_Lzm + PB_zm_Lzp).*dealias;
-                    NL_zeta_Lap = -(0.5).*k2_perp.*PB_zp_zm.*dealias;
-                    NL_zeta_plus  = NL_zeta_Sup - NL_zeta_Lap;
-                    NL_zeta_minus = NL_zeta_Sup + NL_zeta_Lap;
-                    
+                    PB_zp_sp  = fftn((strengthpx(stnn).*sp_y)  - (strengthpy(stnn).*sp_x));
+                    PB_zp_sm  = fftn((strengthpx(stnn).*sm_y)  - (strengthpy(stnn).*sm_x));
+                    PB_zm_sp  = fftn((strengthmx(stnn).*sp_y)  - (strengthmy(stnn).*sp_x));
+                    PB_zm_sm  = fftn((strengthmx(stnn).*sm_y)  - (strengthmy(stnn).*sm_x));
+                                      
                     NL_s_plus   = -(0.5).*((1-bpar(bparnn)).*PB_zp_sp + (1+bpar(bparnn)).*PB_zm_sp).*dealias;
                     NL_s_minus  = -(0.5).*((1+bpar(bparnn)).*PB_zp_sm + (1-bpar(bparnn)).*PB_zm_sm).*dealias;
                     
                     %%% Compute Linear terms
-                    
-                    Lin_zeta_plus  =  va.*KZ.*Lap_z_plus;
-                    Lin_zeta_minus = -va.*KZ.*Lap_z_minus;
+
                     Lin_s_plus     = (va*bpar(bparnn)).*KZ.*s_plus;
                     Lin_s_minus    = -(va*bpar(bparnn)).*KZ.*s_minus;
                     
                     %%% Compute Solution at the next step %%%
-                    if t == dt
-                        Lap_zeta_plus_new  = dt*(Lin_zeta_plus + NL_zeta_plus);% + Lap_z_plus;
-                        Lap_zeta_minus_new = dt*(Lin_zeta_minus + NL_zeta_minus);% + Lap_z_minus;
-                    else
-                        Lap_zeta_plus_new  = 0*dt*(Lin_zeta_plus + NL_zeta_plus) + Lap_z_plus;
-                        Lap_zeta_minus_new = 0*dt*(Lin_zeta_minus + NL_zeta_minus) + Lap_z_minus;
-                        
-                    end
+
                     s_plus_new         = dt*(Lin_s_plus + NL_s_plus) + s_plus;
                     s_minus_new        = dt*(Lin_s_minus + NL_s_minus) + s_minus;
                     
-                    %                     Lap_zeta_plus_new  = Lap_zeta_plus_new.*exp_correct;
-                    %
-                    %                     %%% Forgot to disable this, so field was changing
-                    %                     over time. Could have (probably) screwed with smallscale
-                    %                     wavenumbers
-                    %
-                    %                     Lap_zeta_minus_new = Lap_zeta_minus_new.*exp_correct;
                     s_plus_new  = s_plus_new.*exp_correct;
                     s_minus_new = s_minus_new.*exp_correct;
                     
                     %%% Energy %%%
-                    
-                    E_zeta_plus_grid  = (abs(Lap_zeta_plus_new).^2)./abs(k2_poisson);
-                    E_zeta_minus_grid = (abs(Lap_zeta_minus_new).^2)./abs(k2_poisson);
                     E_s_plus_grid     = (abs(s_plus_new)).^2;
                     E_s_minus_grid    = (abs(s_minus_new)).^2;
-                    
-                    E_zeta_plus(n)  = (0.5)*sum(E_zeta_plus_grid(:))*(grid_int);
-                    E_zeta_minus(n) = (0.5)*sum(E_zeta_minus_grid(:))*(grid_int);
+
                     E_s_plus(n)     = (0.5)*sum(E_s_plus_grid(:))*(grid_int);
                     E_s_minus(n)    = (0.5)*sum(E_s_minus_grid(:))*(grid_int);
                     
@@ -221,19 +136,8 @@ for stnn = 1:length(strengthpx)
                     if (k == TScreen) && m == 0
                         
                         %Go back to real space for plotting
-                        zetap = Lap_zeta_plus_new./k2_poisson;
-                        zetam = Lap_zeta_minus_new./k2_poisson;
-                        zpx = KX.*zetap;
-                        zpy = KY.*zetap;
-                        zmx = KX.*zetam;
-                        zmy = KY.*zetam;
-                        zp  = double(permute(real(ifftn(0.5*(- zpy - zmy))),[2,1,3]));
-                        zm  = double(permute(real(ifftn(0.5*(  zpx + zmx))),[2,1,3]));
-                        disp([mean(zp(:)) mean(zm(:))])
-                        disp([(strengthpy(stnn)-strengthmy(stnn)) strengthpx(stnn)+strengthmx(stnn)])
-%                         disp(zpx)
-% %                                                 zp  = double(permute(real(ifftn(Lap_zeta_plus_new./k2_poisson)),[2,1,3]));
-% %                                                 zm  = double(permute(real(ifftn(Lap_zeta_minus_new./k2_poisson)),[2,1,3]));
+                        
+
 %                         if SlowModes == 1
 %                             sp     = double(permute(real(ifftn(s_plus_new)),[2,1,3]));
 %                             sm     = double(permute(real(ifftn(s_minus_new)),[2,1,3]));
@@ -346,36 +250,35 @@ for stnn = 1:length(strengthpx)
                     u_par = real(ifftn((0.5).*(s_plus_new + s_minus_new)));
                     u_par_save(n)  =  u_par(NX(meshnn)/2, NY(meshnn)/2, NZ(meshnn)/2);
                     
-                    phi = abs((0.5)*((Lap_zeta_plus_new + Lap_zeta_minus_new)./k2_poisson));
-                    phi_x = real(ifftn(KX.*phi)).*dealias;    % y component of u_perp
-                    phi_y = real(ifftn(KY.*phi)).*dealias;    %-x component of u_perp
-                    u_perx_save(n) = -phi_y(NX(meshnn)/2, NY(meshnn)/2, NZ(meshnn)/2);
-                    u_pery_save(n) =  phi_x(NX(meshnn)/2, NY(meshnn)/2, NZ(meshnn)/2);
+%                     phi = abs((0.5)*((Lap_zeta_plus_new + Lap_zeta_minus_new)./k2_poisson));
+%                     phi_x = real(ifftn(KX.*phi)).*dealias;    % y component of u_perp
+%                     phi_y = real(ifftn(KY.*phi)).*dealias;    %-x component of u_perp
+%                     u_perx_save(n) = -phi_y(NX(meshnn)/2, NY(meshnn)/2, NZ(meshnn)/2);
+%                     u_pery_save(n) =  phi_x(NX(meshnn)/2, NY(meshnn)/2, NZ(meshnn)/2);
 %                     disp(toc)
                     k=k+1;
                     n=n+1;
-                    Lap_z_plus  = Lap_zeta_plus_new;
-                    Lap_z_minus = Lap_zeta_minus_new;
+                    
                     s_plus         = s_plus_new;
                     s_minus        = s_minus_new;
                 end
                 %% Calculate omega %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                 disp(n)
-                [velxmax, tvx_max] = findpeaks(abs(u_perx_save),'MinPeakProminence',0.2e-12);
-                
-                [velymax, tvy_max] = findpeaks(abs(u_pery_save),'MinPeakProminence', 0.2e-12);
+%                 [velxmax, tvx_max] = findpeaks(abs(u_perx_save),'MinPeakProminence',0.2e-12);
+%                 
+%                 [velymax, tvy_max] = findpeaks(abs(u_pery_save),'MinPeakProminence', 0.2e-12);
                 
                 [velzmax, tvz_max] = findpeaks(abs(u_par_save),'MinPeakProminence', 0.2e-12);
                 
                 %%% Calculate Omega from vel_x
                 
-                T_nvx = dt*(abs(diff(tvx_max)));
-                Tvx = 2*mean(T_nvx);
-                
-                %%% Calculate Omega from vel_y
-                
-                T_nvy = dt*(abs(diff(tvy_max)));
-                Tvy = 2*mean(T_nvy);
+%                 T_nvx = dt*(abs(diff(tvx_max)));
+%                 Tvx = 2*mean(T_nvx);
+%                 
+%                 %%% Calculate Omega from vel_y
+%                 
+%                 T_nvy = dt*(abs(diff(tvy_max)));
+%                 Tvy = 2*mean(T_nvy);
                 
                 %%% Calculate Omega from vel_z
                 
@@ -388,7 +291,7 @@ for stnn = 1:length(strengthpx)
                     T = nanmean(Tvect);
                     omega = (2*pi)/T;
                 else
-                    Tvect = [Tvx, Tvy, Tvz];    %Manually remove nonlinear components from this vector
+                    Tvect = Tvz;%[Tvx, Tvy, Tvz];    %Manually remove nonlinear components from this vector
                     T = nanmean(Tvect);
                     omega = (2*pi)/T;
                 end
