99baea24fdd3f148dac49983a67889a681025702
Ryan <davry101@student.otago.ac.nz>
2020-07-13 15:58:40 +1200

    VCoefficientAnalysis Updated

--- VCoefficientAnalysis.m
+++ VCoefficientAnalysis.m
@@ -1,7 +1,7 @@
 %% Data Directory %%%
-Directory = './Turbulence/Resolution/128/Unchecked/';
-Folder    = '2020-05-28 15-07-08/';
-Number = 504;     % Number of file we're looking at
+Directory = './Turbulence/';
+Folder    = '2020-07-06 16-05-33/';
+Number = 193;     % Number of file we're looking at
 
 filename = @(n) [Directory Folder sprintf('%u',n) '.mat'];
 
@@ -46,8 +46,8 @@ end
 
 %% Extracting Variables from  Data
 
-sp = ifftn(s_plus);
-sm = ifftn(s_minus);
+sp = real(ifftn(s_plus));
+sm = real(ifftn(s_minus));
 
 delB =  (1/(2*va))*(1+(1/beta)^2)^(-0.5)*(sp - sm);  % deltaB_parallel/B_0
 delU =  (1/(2*va))*(sp + sm);                        % deltaU_parallel/v_A
@@ -56,51 +56,80 @@ delP =  gamma*delR;                                           % Pressure
 
 %% Linear Interpolation
 lstart = [0.8 0.5 0.5];
-ldir = [-0.1 sqrt(0.2) (pi^2)/4]; 
+ldir = [-0.1 sqrt(0.2) (pi^2)]; 
 L = [LX, LY, LZ];
-
-[delB_line, length_line] = Interpolate(delB, dx, LX, lstart, ip, jp, kp, L, ldir);
 [delU_line, length_line] = Interpolate(delU, dx, LX, lstart, ip, jp, kp, L, ldir);
-[delR_line, length_line] = Interpolate(delR, dx, LX, lstart, ip, jp, kp, L, ldir);
-[delP_line, length_line] = Interpolate(delP, dx, LX, lstart, ip, jp, kp, L, ldir);
+[delB_line, length_line] = Interpolate(delB, dx, LX, lstart, ip, jp, kp, L, ldir);
+% [delR_line, length_line] = Interpolate(delR, dx, LX, lstart, ip, jp, kp, L, ldir);
+% [delP_line, length_line] = Interpolate(delP, dx, LX, lstart, ip, jp, kp, L, ldir);
 
 % Coefficient Magnitudes
-% XiMag_line  = rms(delR_line./delB_line); % This isn't right
-% ChiMag_line = rms(delU_line./delB_line);
-% PsiMag_line = rms(delP_line./delB_line);
-
+% XiMag_line  = rms(delR_line)./rms(delB_line);
+ChiMag_line = rms(delU_line)./rms(delB_line);
+% PsiMag_line = rms(delP_line)./rms(delB_line);
+% plot(abs(ChiMag_line))
 % Coefficient Phases
 
-n = length(delB_line); 
-time = [0:(n-1)]*dx;        % 'time' array for wavelet spectrum (note 
-pad = 1;                    % Pads time-series with zeroes for transform
-dj = 0.25;                  % Fraction of sub-octaves per octave
-s0 = 2*dx;                  % Starting scale 
-j1 = 7/dj;                  % Do 7 powers-of-two with dj sub-octaves each
-mother = 'Morlet';          % Wavelet form
+% n = length(delB_line); 
+% time = [0:(n-1)]*dx;        % 'time' array for wavelet spectrum (note actually a length for us)
+% pad = 1;                    % Pads time-series with zeroes for transform
+% dj = 0.25;                  % Fraction of sub-octaves per octave
+% s0 = 2*dx;                  % Starting scale 
+% j1 = 7/dj;                  % Do 7 powers-of-two with dj sub-octaves each
+% mother = 'Morlet';          % Wavelet form
 
-% % Wavelet Transform
-[delB_WT, periodB, scaleB, coiB] = wavelet(delB_line, dx, pad, dj, s0, j1, mother);
-[delU_WT, periodU, scaleU, coiU] = wavelet(delU_line, dx, pad, dj, s0, j1, mother);
-[delR_WT, periodR, scaleR, coiR] = wavelet(delR_line, dx, pad, dj, s0, j1, mother);
-[delP_WT, periodP, scaleP, coiP] = wavelet(delP_line, dx, pad, dj, s0, j1, mother);
+%% Wavelet Transform
 
-C_UB = conj(delU_WT).*delB_WT;
-C_RB = conj(delR_WT).*delB_WT;
-C_PB = conj(delP_WT).*delB_WT;
+% Plot of Wavelet Coherence
+% wcoherence(delU_line,delB_line, seconds(dx), 'PhaseDisplayThreshold', 0.5)
+% wcoherence(delU_line,delB_line, 'PhaseDisplayThreshold', 0.2);
+[wcohUB, wcsUB, period, coi] = wcoherence(delU_line,delB_line, seconds(dx), 'PhaseDisplayThreshold', 0.2);
 
-C_UB_Phase = atan( imag(C_UB)./real(C_UB));
-C_RB_Phase = atan( imag(C_RB)./real(C_RB));
-C_PB_Phase = atan( imag(C_PB)./real(C_PB));
+% Trim off scales larger than LX/2 to avoid measuring coherence of 'same
+% point'
+period = seconds(period);
+smallscale = find(period<(LX/2));
+wcsUBsmall = wcsUB(smallscale, :);
+wcohUBsmall = wcohUB(smallscale, :);
+periodsmall = period(smallscale);
 
+UBphase = angle(wcsUBsmall);
+bins = linspace(-pi, pi, 51);
+% histUB = zeros(51, length(smallscale));
+figure
+hold on
+for i = smallscale'
+    disp(i)
+    UBphasei = UBphase(i,:);
+    wcohUBi  = wcohUBsmall(i,:);
+    histUB = histwv(UBphasei, wcohUBi, -pi, pi, 51);
+    histUB = histUB./max(histUB);
+    barh(bins,histUB) 
+    drawnow
+    pause(0.5)
+end
+% figure
+% subplot(1,2,1)
+% semilogx(period, UBphaseW, 'o')
+% title(['\beta = ' num2str(beta)])
+% subplot(1,2,2)
+% 
+% [histw histv] = histwv(meanscaleUB, WEIGHT, -pi, pi, 31);
+% barh(bins, histw)
 
+% histogram(meanscaleUB)% figure(2)
+
+% axis([0 2 -pi pi])
+% wcoherence(delR_line, delB_line)
+% figure(3)
+% wcoherence(delP_line, delB_line)
 %% Interpolation Function
 
 function [out, lvec] = Interpolate(delA, dx, LX, lstart, i, j, k, L, ldir)
 
 ldir = ldir/norm(ldir); % Direction along which to look
 dl = dx;
-total_length = LX^2 ;
+total_length = 10000*LX^2 ;
 lvec = [(-total_length/2):dl:(total_length/2 -dl)];
 clear plist
 plist = zeros(1,length(lvec));
@@ -112,5 +141,24 @@ end
 % Interpolate to points
 itype = 'linear';
 out = interpn(i, j, k,padBoundaries(delA), plist(1,:), plist(2,:), plist(3,:), itype, NaN).';
+end
+
+%% Weighted Histogram
+
+function histw = histwv(v, w, min, max, bins)
+%Inputs:
+% v - values
+% w - weights
+% min - minimum value
+% max - max value
+% bins - number of bins (inclusive)
+
+%Outputs:
+%histw - wieghted histogram
+%histv (optional) - histogram of values
+
+delta = (max-min)/(bins-1);
+subs = round((v-min)/delta)+1;
+histw = accumarray(subs',w',[bins,1]);
 end
 
